61b1409e6e8f44fc82b6b110bb829041d5391d78
bishal1877 <bishalburnwal1877@gmail.com>
2026-01-29 17:37:39 +0530

    Adjust div height calculation in Page component

--- frontend/src/app/chat/[slug]/page.js
+++ frontend/src/app/chat/[slug]/page.js
@@ -5,7 +5,7 @@ import Header from "@/app/component/Header/Header";
 export default async function Page({params}) {
   const { slug } = await params;
   
-  return <div style={{color:"white",backgroundColor:"white" ,margin:"auto",width:"100%",marginTop:"1px" ,border:"3px solid white", borderRadius:"0px 0px 0px 0px",height:"91vh",position:"relative",zIndex:"3"}} > 
+  return <div style={{color:"white",backgroundColor:"white" ,margin:"auto",width:"100%",marginTop:"1px" ,border:"3px solid white", borderRadius:"0px 0px 0px 0px", height: "calc(100vh - 70px)",position:"relative",zIndex:"3"}} > 
   <Header  heading={slug} />
   <Chat room={slug} />
   </div>;
